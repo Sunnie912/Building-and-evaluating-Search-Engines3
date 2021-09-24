8d7a4775c747b3ce9e8685d342476858bb539a27
Sunnie912 <wangdf92@gmail.com>
2021-09-24 15:46:31 -0700

    i1

--- search_eval.py
+++ search_eval.py
@@ -57,7 +57,7 @@ def load_ranker(cfg_file):
     #return metapy.index.OkapiBM25(k1=1.525,b=0.75, k3=3.235)
     #return metapy.index.OkapiBM25(k1=1.525,b=0.75, k3=4.235)
     #return metapy.index.PivotedLength(0.9)
-    return metapy.index.JelinekMercer(0.7)
+    return metapy.index.JelinekMercer(700)
 
 if __name__ == '__main__':
     if len(sys.argv) != 2:
